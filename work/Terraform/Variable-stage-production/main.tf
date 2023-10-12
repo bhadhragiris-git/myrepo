b67bb60467b70c2993bea107cbd5227cc43ddede
bhadhragiris-git <bhadhragiris@gmail.com>
2023-10-12 19:51:54 +0530

    added

---
+++ work/Terraform/Variable-stage-production/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
+
+resource "aws_instance" "my_ec2_example_instance" {
+    ami = "ami-00abfd8da690f984d"  
+    instance_type = var.instance_type 
+    //count = var.instance_count
+    count = 1
+    associate_public_ip_address = var.enable_public_ip
+    /*tags = {
+        Name = "Terraform New EC2 Instance"
+    }*/
+    tags =  {
+        Name = var.environment_type
+    }
+}
+
+
+resource "aws_iam_user" "example" {
+  count = length(var.user_names)
+  name  = var.user_names[count.index]
+}
+

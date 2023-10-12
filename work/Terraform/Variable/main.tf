b216095925335bb37aa934360fda81c385046037
bhadhragiris-git <bhadhragiris@gmail.com>
2023-10-12 18:19:07 +0530

    added variable

--- work/Terraform/Variable/main.tf
+++ work/Terraform/Variable/main.tf
@@ -16,40 +16,9 @@ resource "aws_instance" "my_ec2_example_instance" {
     tags = var.project_environment
 }
 
-variable "instance_type" {
-   description = "Instance type t2.micro"
-   type        = string
-   default     = "t2.micro"
-} 
-
-/*variable "instance_count" {
-  description = "EC2 instance count"
-  type        = number
-  default     = 2
-}*/
-
-variable "enable_public_ip" {
-  description = "Enable public IP address"
-  type        = bool
-  default     = true
-}
 
 resource "aws_iam_user" "example" {
   count = length(var.user_names)
   name  = var.user_names[count.index]
 }
 
-variable "user_names" {
-  description = "IAM usernames"
-  type        = list(string)
-  default     = ["user1", "user2", "user3s"]
-}
-
-variable "project_environment" {
-  description = "project name and environment"
-  type        = map(string)
-  default     = {
-    project     = "project-alpha",
-    environment = "dev"
-  }
-}

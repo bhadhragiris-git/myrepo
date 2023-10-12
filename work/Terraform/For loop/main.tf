66685efa75722a4804325ed429882252a6dd659f
bhadhragiris-git <bhadhragiris@gmail.com>
2023-10-12 21:25:38 +0530

    added

---
+++ work/Terraform/For loop/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
+
+resource "aws_instance" "my_ec2_example_instance" {
+    ami = "ami-00abfd8da690f984d"  
+    instance_type = "t2.micro" 
+    tags = {
+        Name = "Terraform New EC2 Instance"
+    }
+}
+
+resource "aws_iam_user" "example" {
+  for_each = var.user_names
+  name  = each.value
+}
+
+variable "user_names" {
+  description = "IAM usernames"
+  type        = set(string)
+  default     = ["user1", "user2", "user3"]
+}

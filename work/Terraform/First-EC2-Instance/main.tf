351124f62339046df896deb9806168084e466732
bhadhragiris-git <bhadhragiris@gmail.com>
2023-10-12 15:41:53 +0530

    added TF code

--- work/Terraform/First-EC2-Instance/main.tf
+++ work/Terraform/First-EC2-Instance/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAVE2XXGKKLKA54RU2"
-  secret_key = "921ayARIoTGSO2qQ6h7bhFZ5ZkWc0bzS8ldzygN6"
+  access_key = ""
+  secret_key = ""
 }
 
 resource "aws_instance" "my_ec2_example_instance" {

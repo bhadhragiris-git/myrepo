96962942fb84aea0994669f70c5fce55c86e71b8
bhadhragiris-git <bhadhragiris@gmail.com>
2023-10-12 17:51:54 +0530

    added variable

--- work/Terraform/Variable/main.tf
+++ work/Terraform/Variable/main.tf
@@ -7,7 +7,8 @@ provider "aws" {
 resource "aws_instance" "my_ec2_example_instance" {
     ami = "ami-00abfd8da690f984d"  
     instance_type = var.instance_type 
-    count = var.instance_count
+    //count = var.instance_count
+    count = 1
     associate_public_ip_address = var.enable_public_ip
     tags = {
         Name = "Terraform New EC2 Instance"
@@ -20,14 +21,25 @@ variable "instance_type" {
    default     = "t2.micro"
 } 
 
-variable "instance_count" {
+/*variable "instance_count" {
   description = "EC2 instance count"
   type        = number
   default     = 2
-}
+}*/
 
 variable "enable_public_ip" {
   description = "Enable public IP address"
   type        = bool
   default     = true
+}
+
+resource "aws_iam_user" "example" {
+  count = length(var.user_names)
+  name  = var.user_names[count.index]
+}
+
+variable "user_names" {
+  description = "IAM usernames"
+  type        = list(string)
+  default     = ["user1", "user2", "user3s"]
 }

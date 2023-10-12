94fae65d73266a624eb004042ab9bb1e2ba1daf9
bhadhragiris-git <bhadhragiris@gmail.com>
2023-10-12 21:01:15 +0530

    added output

---
+++ work/Terraform/Terraform-output/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region     = "us-east-1"
+    access_key = ""
+    secret_key = ""
+}
+
+resource "aws_instance" "my_ec2_example_instance" {
+    ami = "ami-00abfd8da690f984d"  
+    instance_type = "t2.micro" 
+    tags = {
+        Name = "Terraform New EC2 Instance"
+    }
+}
+
+output "console output" {
+   value = "First EC2 Instance"
+}

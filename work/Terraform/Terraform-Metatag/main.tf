0f89f0c10e7a0cbad6123e54ed841af55f52d574
bhadhragiris-git <bhadhragiris@gmail.com>
2023-10-13 16:58:10 +0530

    added

---
+++ work/Terraform/Terraform-Metatag/main.tf
@@ -0,0 +1,26 @@
+
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
+
+resource "aws_s3_bucket" "my_test_bucket"{
+     bucket = "import-test-bucket"
+
+     tags {
+
+        "name" = "test-bucket" 
+     }
+}
+resource "aws_instance" "my_ec2_example_instance" {
+    ami = "ami-00abfd8da690f984d"  
+    instance_type = "t2.micro" 
+    tags = {
+        Name = "Terraform New EC2 Instance"
+    }
+
+    depends_on = [aws_s3_bucket.my_test_bucket]
+}
+

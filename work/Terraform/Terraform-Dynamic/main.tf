226ddcbf95742764cdaf3514d1880369c92d1a78
bhadhragiris-git <bhadhragiris@gmail.com>
2023-10-13 13:11:09 +0530

    added

--- work/Terraform/Terraform-Dynamic/main.tf
+++ work/Terraform/Terraform-Dynamic/main.tf
@@ -10,9 +10,21 @@ resource "aws_instance" "ec2_example" {
 
     ami = "ami-0767046d1677be5a0"  
     instance_type = "t2.micro" 
+
+    key_name= "aws_key"
+    vpc_security_group_ids = [aws_security_group.main.id]
+
     tags = {
         Name = "Terraform EC2"
     }
+
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = file("local-machine-path/keys/aws/aws_key")
+      timeout     = "4m"
+   }
 }
 
 locals {
@@ -45,4 +57,9 @@ resource "aws_security_group" "main" {
    tags = {
       Name = "AWS security group dynamic block"
    }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "aws_key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDbvRN/gvQBhFe+dE8p3Q865T/xTKgjqTjj56p1IIKbq8SDyOybE8ia0rMPcBLAKds+wjePIYpTtRxT9UsUbZJTgF+SGSG2dC6+ohCQpi6F3xM7ryL9fy3BNCT5aPrwbR862jcOIfv7R1xVfH8OS0WZa8DpVy5kTeutsuH5FMAmEgba4KhYLTzIdhM7UKJvNoUMRBaxAqIAThqH9Vt/iR1WpXgazoPw6dyPssa7ye6tUPRipmPTZukfpxcPlsqytXWlXm7R89xAY9OXkdPPVsrQA0XFQnY8aFb9XaZP8cm7EOVRdxMsA1DyWMVZOTjhBwCHfEIGoePAS3jFMqQjGWQd rahul@rahul-HP-ZBook-15-G2"
 }
